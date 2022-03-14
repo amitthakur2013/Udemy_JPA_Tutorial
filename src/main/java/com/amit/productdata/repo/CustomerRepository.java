c94d02388319b393e67f3e19f338521ee299602b
amitthakur2013 <amitthakur2013@gmail.com>
2022-03-14 14:10:29 +0530

    assignment done

--- src/main/java/com/amit/productdata/repo/CustomerRepository.java
+++ src/main/java/com/amit/productdata/repo/CustomerRepository.java
@@ -2,7 +2,11 @@ package com.amit.productdata.repo;
 
 import java.util.List;
 
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.amit.productdata.model.Customer;
@@ -11,5 +15,9 @@ import com.amit.productdata.model.Customer;
 public interface CustomerRepository extends CrudRepository<Customer, Integer>{
 	List<Customer> findByNameAndEmail(String name, String email);
 	List<Customer> findByEmailLike(String email);
-	List<Customer> findByIdIn(List<Integer> ids);
+	List<Customer> findByIdIn(List<Integer> ids, Pageable pageable);
+	
+	@Modifying
+	@Query("update Customer set email=:email where id=:id")
+	void updateCustomerEmail(@Param("id")int id,@Param("email") String email);
 }
--- src/test/java/com/amit/productdata/CustomerTests.java
+++ src/test/java/com/amit/productdata/CustomerTests.java
@@ -3,6 +3,8 @@ package com.amit.productdata;
 import java.util.Arrays;
 import java.util.List;
 
+import javax.transaction.Transactional;
+
 import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
@@ -11,6 +13,10 @@ import org.junit.jupiter.api.TestInstance.Lifecycle;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.test.annotation.Rollback;
 
 import com.amit.productdata.model.Customer;
 import com.amit.productdata.repo.CustomerRepository;
@@ -54,8 +60,18 @@ class CustomerTests {
 	@Test
 	@Order(4)
 	public void testfindByIdIn() {
-		List<Customer> c = repository.findByIdIn(Arrays.asList(1,3,4,5,6,7));
+		List<Customer> c = repository.findByIdIn(Arrays.asList(1,3,4,5,6,7),PageRequest.of(2, 2,Sort.by(Direction.DESC,"id")));
 		System.out.println(c);
 	}
+	
+	@Test
+	@Order(5)
+	@Transactional
+	@Rollback(false)
+	public void testUpdateEmail() {
+		repository.updateCustomerEmail(5, "ankur@gmail.com");
+	}
+	
+	
 
 }
